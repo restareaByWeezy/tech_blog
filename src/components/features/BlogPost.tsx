934a94cc4db7ef49632942bf5bc0f512e3dba3bc
restareaByWeezy <fjgkd7777@naver.com>
2023-04-04 22:41:45 +0900

    🐛 Modify(minor): minor fix

--- src/components/features/BlogPost.tsx
+++ src/components/features/BlogPost.tsx
@@ -11,7 +11,6 @@ interface BlogPostProps {
 }
 
 const BlogPost = ({ post }: BlogPostProps) => {
-  console.log('🚀 ~ file: BlogPost.tsx:14 ~ BlogPost ~ post:', post);
   return (
     <Link href={`/blog/${post._raw.flattenedPath}`} key={post._id} passHref>
       <div className={styles.glowParent}>
